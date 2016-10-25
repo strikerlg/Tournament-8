829c064b42f508f4130e6174761f81768aff9d05
pstryk <godlovesu@wp.pl>
2016-10-25 23:15:53 +0200

    further tests of LeagueParticipantEventListener

--- src/Pstryk82/LeagueBundle/Tests/Domain/ReadModel/Listener/LeagueParticipantEventListenerTest.php
+++ src/Pstryk82/LeagueBundle/Tests/Domain/ReadModel/Listener/LeagueParticipantEventListenerTest.php
@@ -3,13 +3,18 @@
 namespace Pstryk82\LeagueBundle\Tests\Domain\ReadModel\Listener;
 
 use Pstryk82\LeagueBundle\Domain\Aggregate\Competition;
+use Pstryk82\LeagueBundle\Domain\Aggregate\League;
+use Pstryk82\LeagueBundle\Domain\Aggregate\LeagueParticipant;
 use Pstryk82\LeagueBundle\Domain\Aggregate\Team;
+use Pstryk82\LeagueBundle\Domain\Logic\GameOutcomeResolver;
 use Pstryk82\LeagueBundle\Domain\ReadModel\Listener\LeagueParticipantEventListener;
-use Pstryk82\LeagueBundle\Domain\ReadModel\Projection\CompetitionProjection;
 use Pstryk82\LeagueBundle\Domain\ReadModel\Projection\LeagueParticipantProjection;
 use Pstryk82\LeagueBundle\Domain\ReadModel\Projection\LeagueProjection;
 use Pstryk82\LeagueBundle\Domain\ReadModel\Projection\TeamProjection;
 use Pstryk82\LeagueBundle\Event\LeagueParticipantWasCreated;
+use Pstryk82\LeagueBundle\Event\ParticipantHasDrawn;
+use Pstryk82\LeagueBundle\Event\ParticipantHasLost;
+use Pstryk82\LeagueBundle\Event\ParticipantHasWon;
 
 class LeagueParticipantEventListenerTest extends AbstractEventListnerTest
 {
@@ -52,5 +57,87 @@ class LeagueParticipantEventListenerTest extends AbstractEventListnerTest
         $this->assertProjectionSaved($leagueParticipant, 2);
 
         $this->listener->when($event);
+    }
+
+    public function testOnParticipantHasWon()
+    {
+        $competition = $this->getMockBuilder(League::class)->disableOriginalConstructor()->getMock();
+        $competition->method('getAggregateId')->willReturn('league');
+        $competition->method('getPointsForWin')->willReturn(3);
+        $participant = $this->getMockBuilder(LeagueParticipant::class)->disableOriginalConstructor()->getMock();
+        $participant->method('getAggregateId')->willReturn('participant');
+        $gameOutcomeResolver = $this->getMockBuilder(GameOutcomeResolver::class)->disableOriginalConstructor()->getMock();
+        $gameOutcomeResolver->method('getWinner')->willReturn($participant);
+        $gameOutcomeResolver->method('getWinnerScore')->willReturn(4);
+        $gameOutcomeResolver->method('getLoserScore')->willReturn(1);
+        $event = new ParticipantHasWon($competition, $gameOutcomeResolver);
+
+        $participantProjection = new LeagueParticipantProjection('participant');
+        $this->assertProjectionFound($participantProjection, LeagueParticipantProjection::class, 0);
+
+        $leagueProjection = new LeagueProjection('league');
+        $leagueProjection->setPointsForWin(5);
+        $this->assertProjectionFound($leagueProjection, LeagueProjection::class, 1);
+
+        $this->assertProjectionSaved($participantProjection, 2);
+        $this->listener->when($event);
+
+        $this->assertEquals(5, $participantProjection->getPoints());
+        $this->assertEquals(4, $participantProjection->getGoalsFor());
+        $this->assertEquals(1, $participantProjection->getGoalsAgainst());
+        $this->assertEquals(3, $participantProjection->getGoalDifference());
+    }
+
+    public function testOnParticipantHasLost()
+    {
+        $competition = $this->getMockBuilder(League::class)->disableOriginalConstructor()->getMock();
+        $competition->method('getAggregateId')->willReturn('league');
+        $participant = $this->getMockBuilder(LeagueParticipant::class)->disableOriginalConstructor()->getMock();
+        $participant->method('getAggregateId')->willReturn('participant');
+        $gameOutcomeResolver = $this->getMockBuilder(GameOutcomeResolver::class)->disableOriginalConstructor()->getMock();
+        $gameOutcomeResolver->method('getLoser')->willReturn($participant);
+        $gameOutcomeResolver->method('getWinnerScore')->willReturn(4);
+        $gameOutcomeResolver->method('getLoserScore')->willReturn(1);
+        $event = new ParticipantHasLost($competition, $gameOutcomeResolver);
+
+        $participantProjection = new LeagueParticipantProjection('participant');
+        $this->assertProjectionFound($participantProjection, LeagueParticipantProjection::class, 0);
+
+        $leagueProjection = new LeagueProjection('league');
+        $leagueProjection->setPointsForLose(-1);
+        $this->assertProjectionFound($leagueProjection, LeagueProjection::class, 1);
+
+        $this->assertProjectionSaved($participantProjection, 2);
+        $this->listener->when($event);
+
+        $this->assertEquals(-1, $participantProjection->getPoints());
+        $this->assertEquals(1, $participantProjection->getGoalsFor());
+        $this->assertEquals(4, $participantProjection->getGoalsAgainst());
+        $this->assertEquals(-3, $participantProjection->getGoalDifference());
+    }
+
+    public function testOnParticipantHasDrawn()
+    {
+        $competition = $this->getMockBuilder(League::class)->disableOriginalConstructor()->getMock();
+        $competition->method('getAggregateId')->willReturn('league');
+        $competition->method('getPointsForWin')->willReturn(3);
+        $participant = $this->getMockBuilder(LeagueParticipant::class)->disableOriginalConstructor()->getMock();
+        $participant->method('getAggregateId')->willReturn('participant');
+        $event = new ParticipantHasDrawn($competition, 'participant', 2);
+
+        $participantProjection = new LeagueParticipantProjection('participant');
+        $this->assertProjectionFound($participantProjection, LeagueParticipantProjection::class, 0);
+
+        $leagueProjection = new LeagueProjection('league');
+        $leagueProjection->setPointsForDraw(2);
+        $this->assertProjectionFound($leagueProjection, LeagueProjection::class, 1);
+
+        $this->assertProjectionSaved($participantProjection, 2);
+        $this->listener->when($event);
+
+        $this->assertEquals(2, $participantProjection->getPoints());
+        $this->assertEquals(2, $participantProjection->getGoalsFor());
+        $this->assertEquals(2, $participantProjection->getGoalsAgainst());
+        $this->assertEquals(0, $participantProjection->getGoalDifference());
     }
 }
